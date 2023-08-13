d70cb519d7ba7370ac3d3dbb0c4eae6682bc2562
Vadim Kosarev <mailkosarev@gmail.com>
2023-08-13 17:20:22 +0400

    feature: header css and html

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__copy">&copy; {new Date().getFullYear()} Mesto Russia</p>
+      <p className="footer__copy">&copy; {new Date().getFullYear()} BeatFilm</p>
     </footer>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,15 +6,26 @@ import './Header.css';
 function Header() {
   return (
     <header className="header">
-      <div className="header__nav">
-        <div className="header__left-column">
-          <Link to="/">
-            <div className="header__logo"></div>
-          </Link>
-        </div>
-        <div className="header__right-column">
-
-        </div>
+      <div className="header__column header__column_nav">
+        <Link to="/">
+          <div className="header__logo"></div>
+        </Link>
+        <Link className="link header__link" to="/films">
+          Фильмы
+        </Link>
+        <Link className="link header__link" to="/favourite">
+          Сохранённые фильмы
+        </Link>
+      </div>
+      <div className="header__column header__column_account">
+        <Link className="link header__link" to="/signup">
+          Регистрация
+        </Link>
+        <Link to="/signin">
+          <button className="button button_color_blue header__button" type="button">
+            Войти
+          </button>
+        </Link>
       </div>
     </header>
   );
