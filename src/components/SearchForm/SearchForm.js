fe1a2993f791e743071b732de8d8233ba2c08d12
Vadim Kosarev <mailkosarev@gmail.com>
2023-09-18 23:48:16 +0400

    feat: middle of SearchForm component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -32,12 +32,12 @@ function SearchForm({ handleSubmitSearch }) {
         method="post"
         autoComplete="off"
       >
-        <label htmlFor="searchInput" className="search__icon"></label>
+        <label htmlFor="searchLine" className="search__icon"></label>
         <input
           value={value}
           onChange={handleSearchChange}
-          id="searchInput"
-          className="search__input"
+          id="searchLine"
+          className="search__input search__input_el_search-line"
           type="search"
           name="search"
           required
@@ -45,22 +45,19 @@ function SearchForm({ handleSubmitSearch }) {
           minLength="2"
           maxLength="100"
         />
-        <button className="button search__submit" type="submit">
-          {/* <svg width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <rect width="34" height="34" rx="17" fill="#3456F3" />
-            <path fillRule="evenodd" clipRule="evenodd" d="M18.7927 18.2638C17.3608 19.6957 15.0391 19.6957 13.6072 18.2638C12.1753 16.8319 12.1753 14.5103 13.6072 13.0783C15.0391 11.6464 17.3608 11.6464 18.7927 13.0783C20.2246 14.5103 20.2246 16.8319 18.7927 18.2638ZM19.2331 19.6468C17.2728 21.1462 14.4572 20.9994 12.6644 19.2066C10.7118 17.254 10.7118 14.0882 12.6644 12.1355C14.617 10.1829 17.7829 10.1829 19.7355 12.1355C21.5282 13.9283 21.675 16.7437 20.1758 18.7039L23.7425 22.2706L22.7997 23.2134L19.2331 19.6468Z" fill="white" />
-          </svg> */}
-        </button>
-        <input
+        <button className="button search__submit" type="submit"></button>
+      </form>
+      <label htmlFor="isShortFilm" className="search__label search__label_el_extra-options">
+        <input className="search__input search__input_el_extra-options"
           value={isShortFilm}
           onChange={handleIsShortFilmChange}
           id="isShortFilm"
-          className="search__checkbox"
           type="checkbox"
           name="isShortFilm"
         />
-        <label htmlFor="isShortFilm" className="search__input-label">Короткометражки</label>
-      </form>
+        <span className="search__pseudo-item search__pseudo-item_type_checkbox"></span>
+        <span className="search__label-text">Короткометражки</span>
+      </label>
     </section>
   )
 }
